0ca78a5538d54b01772436b8506398fd6d846f16
Administrator <yyy1471876052@163.com>
2016-03-10 15:14:49 +0800

    测试

---
+++ JavaDemo02/src/com/bbs/test/Test.java
@@ -0,0 +1,24 @@
+package com.bbs.test;
+
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2016/3/10.
+ */
+public class Test {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int d = scanner.nextInt();
+        switch (d){
+            case 0 :
+                System.out.print("您好，周末");
+                break;
+            case (1):
+                System.out.print("哎，有到工作日");
+                break;
+            default:
+                System.out.print("输入错误");
+                break;
+        }
+    }
+}
